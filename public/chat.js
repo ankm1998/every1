310d4d649db15f4028fbbeb33f9a99968f15d7d8
aash narayan mahato <ankm786@gmail.com>
2017-11-18 19:09:16 +0530

    first commit

---
+++ public/chat.js
@@ -0,0 +1,34 @@
+// Make connection
+var socket = io.connect('http://narayan-chat.herokuapp.com');
+
+// Query DOM
+var message = document.getElementById('message'),
+    handle = document.getElementById('handle'),
+    btn = document.getElementById('send'),
+    output = document.getElementById('output'),
+    feedback = document.getElementById('feedback');
+
+// Emit events
+btn.addEventListener('click', function(){
+  socket.emit('chat', {
+      message: message.value,
+      handle: handle.value
+  });
+});
+
+// Handle Input
+                message.addEventListener('keydown', function(event){
+                    if(event.which === 13 && event.shiftKey == false){
+                        // Emit to server input
+                        socket.emit('typing',handle.value);
+              }
+        });
+// Listen for events
+socket.on('chat', function(data){
+  feedback.innerHTML="";
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing',function(data){
+  feedback.innerHTML='<p><em>'+data+'is typing a message...</em></p>';
+});
