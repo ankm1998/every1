4c5f4668b0c5fd2bfcf6657aa01440158a96de96
secret <32462580+ankm1998@users.noreply.github.com>
2017-11-29 23:48:01 +0530

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 // Make connection
-var socket = io.connect('http://every1schat.herokuapp.com'); 
+var socket = io.connect('http://every1schat1.herokuapp.com'); 
 
 // Query DOM
 var message = document.getElementById('message'),
